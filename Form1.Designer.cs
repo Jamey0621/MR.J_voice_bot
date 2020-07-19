a6cf34ab848f222b46fbd6457cebd9e7ca07c6bc
Jamey Geoghagan <Jameygeoghagan@gmail.com>
2020-07-19 15:50:00 -0500

    Made it, need to refactor it and add more to it

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace voiceBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(659, 604);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(659, 604);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(659, 604);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 603);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(673, 642);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mr. J";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+
+namespace voiceBot
+{
+    public partial class Form1 : Form
+    {
+        // inti it
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        //seting up the speech commands
+        Choices list = new Choices();
+        // name is list for the commands
+
+        bool wake = true;
+        public Form1()
+        {
+            // int speech rec
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+
+               // will add these to a text file
+            list.Add(new string[]{"hi how are you", "tell me about your self","what time is it","what is today","i need to look up something","open youtube","i need to do some code wars",
+            "sleep","j","open spotify","open windows media player", "play", "pause","next song"
+            });
+
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+
+
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammarAsync(gr);
+                rec.SpeechRecognized += Rec_SpeechRecognized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch { return; }
+          
+
+
+
+            // testing to make sure it works
+           // s.Speak("Test");
+             // setting the gender of my vocie
+            s.SelectVoiceByHints(VoiceGender.Neutral);
+            s.SpeakAsync("Hello");
+            s.SpeakAsync("I am Mr. J.");
+           
+           
+            InitializeComponent();
+        }
+
+        
+
+
+        public void say( string com)
+        {
+            s.Speak(com);
+        }
+        private void Rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string r = e.Result.Text;
+            // wake and sleep
+            if(r == "j")
+            {
+                say("What can i do for yo?");
+                wake = true;
+            }
+            else if(r == "sleep")
+            {
+                say("If you need me say hey J");
+                wake = false;
+            }
+
+
+            if (wake == true)
+            {
+                // command
+                if (r == "hi how are you")
+                {
+                    // respond
+                    say("I am great, how are you?");
+                }
+                if (r == "tell me about your self")
+                {
+                    say("I am a bot made by Jamey");
+                }
+                if (r == "what time is it")
+                {
+                    say(DateTime.Now.ToString("h mm tt "));
+                }
+                if (r == "what is today")
+                {
+                    say(DateTime.Now.ToString("M/d/yyyy "));
+                }
+               
+                if (r == "i need to look up something")
+                {
+                    say("i will open up google for you");
+                    Process.Start("http://google.com");
+                }
+                if (r == "open youtube")
+                {
+                    Process.Start("http://youtube.com");
+                }
+                if (r == "i need to do some code wars")
+                {
+                    say("get to work");
+                    Process.Start("http://www.codewars.com");
+                }
+                if( r == "open spotify")
+                {
+                    Process.Start(@"Spotify.exe");
+                }
+                if(r == "play" || r == "pause")
+                {
+                    SendKeys.Send(" ");
+                }
+                if( r == "next song")
+                {
+                    SendKeys.Send("^{RIGHT}^");
+                }
+                    
+                    
+                if(r == "open windows media player")
+                {
+                    Process.Start(@"wmplayer.exe");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
